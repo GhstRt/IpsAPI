f47c075cdc5f2fa821c1c509e2c3ef17f86e981d
Yunus Emre Öztürk <31356491+GhstRt@users.noreply.github.com>
2024-04-07 03:33:32 +0300

    Fixed bugs

--- api/views/Login.py
+++ api/views/Login.py
@@ -11,8 +11,8 @@ class Login(APIView):
     def post(self, request):
         ip = request.headers.get("REMOTE_ADDR")
         try:
-            username = request.data.get("username")
-            password = request.data.get("password")
+            username = request.data["username"]
+            password = request.data["password"]
             user = User.objects.filter(username=username, password=password)
             token = self.generate_random_token(user)
             return Response({"status": {"code": "0", "message": "success"}, "data": {"user": user, "token": token}},
